edd8bae5586ba5ba60644f84240e62d7af5e8216
Dave Dankel <ddankel@gmail.com>
2021-11-29 22:24:48 -0600

    Updated schema

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,20 +1,21 @@
 exports.createSchemaCustomization = ({ actions }) => {
   const { createTypes } = actions;
   const typeDefs = `
-  type MarkdownRemark implements Node {
+  type MarkdownRemark implements Node @infer {
     frontmatter: Frontmatter!
   }
-  type Frontmatter {
+  type Frontmatter @infer {
     slug: String!
     title: String
     sku: String
     name: String
     line: [String]
     painted: String
-    paints: [String]
     weapons: [String]
     armor: [String]
     race: [String]
+    recipes: [String]
+    status: String
   }
   `;
   createTypes(typeDefs);
