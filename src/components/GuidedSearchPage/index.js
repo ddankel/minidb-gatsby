9ff3e1904421649165bbb223b363121e5f7181a0
Dave Dankel <ddankel@gmail.com>
2022-07-15 20:36:57 -0500

    Improve guided search page layout

--- src/components/GuidedSearchPage/index.js
+++ src/components/GuidedSearchPage/index.js
@@ -1,43 +1,33 @@
 import React from "react";
 import styled from "styled-components";
-import { Row, Col } from "react-bootstrap";
 
-import Filters from "./Filters";
 import Gallery from "./Gallery";
 import { useFilterContext } from "../../context/FilterContext";
+import MobileMenu from "./MobileMenu";
+import DesktopMenu from "./DesktopMenu";
 
-const Spacer = styled.div`
-  height: 3rem;
+const FlexContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
 `;
 
-const Menu = styled.div`
-  margin-top: -33px;
-  position: sticky;
-  top: 0;
+const GalleryContainer = styled.div`
+  flex-grow: 1;
 `;
-
-const SmFilterContainer = styled.div.attrs({
-  className: "d-md-none mx-3",
-})``;
 
 const GuidedSearchPage = () => {
   const { filteredMiniatures } = useFilterContext();
 
   return (
-    <Row>
-      <Col md={4} lg={3} className="d-none d-md-block">
-        <Menu>
-          <Filters btnClass="w-100" />
-        </Menu>
-      </Col>
-      <Col sm={12} md={8} lg={9}>
-        <SmFilterContainer>
-          <Filters btnClass="float-end" />
-          <Spacer />
-        </SmFilterContainer>
-        <Gallery minis={filteredMiniatures} />
-      </Col>
-    </Row>
+    <>
+      <MobileMenu />
+      <FlexContainer>
+        <DesktopMenu />
+        <GalleryContainer>
+          <Gallery minis={filteredMiniatures} />
+        </GalleryContainer>
+      </FlexContainer>
+    </>
   );
 };
 
