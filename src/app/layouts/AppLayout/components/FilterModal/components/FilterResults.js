581e720b76be0921182a8afb0aaded393e6e1703
Dave Dankel <ddankel@gmail.com>
2023-06-13 23:53:21 -0500

    Add counts

--- src/app/layouts/AppLayout/components/FilterModal/components/FilterResults.js
+++ src/app/layouts/AppLayout/components/FilterModal/components/FilterResults.js
@@ -1,3 +1,4 @@
+import { useFilteredCollectionZippedTags } from "@/modules/gallery/hooks/useAggregatedTags";
 import { startCase } from "lodash";
 
 const FilterResults = ({ option }) => {
@@ -6,9 +7,42 @@ const FilterResults = ({ option }) => {
     .map((segment) => startCase(segment))
     .join(" > ");
 
+  const zippedTags = useFilteredCollectionZippedTags();
+
+  // console.log("!!!!", zippedTags);
+
+  const getCountForTag = (type, value) => {
+    let tagsForType = [];
+    switch (type) {
+      case "archetype":
+        tagsForType = zippedTags.archetypeTags;
+        break;
+      case "armor":
+        tagsForType = zippedTags.armorTags;
+        break;
+      case "lines":
+        tagsForType = zippedTags.lines;
+        break;
+      case "painted":
+        tagsForType = zippedTags.paintedTags;
+        break;
+      case "race":
+      case "species":
+        tagsForType = zippedTags.raceTags;
+        break;
+      case "weapon":
+        tagsForType = zippedTags.weaponTags;
+        break;
+    }
+
+    const matchingTag = tagsForType.find((item) => item.name === value);
+
+    return !!matchingTag ? matchingTag.count : "";
+  };
+
   return (
     <span>
-      {startCase(option.type)}: {formattedValue} (TODO COUNT)
+      {startCase(option.type)}: {formattedValue} ({getCountForTag(option.type, option.value)})
     </span>
   );
 };
