ba73125e2027c2d08ccebda67ad5948759816ee6
Dave Dankel <ddankel@gmail.com>
2023-06-15 09:29:12 -0500

    Added exports to store

--- src/common/hooks/useAggregationStore/index.js
+++ src/common/hooks/useAggregationStore/index.js
@@ -1,24 +1,17 @@
 import { create } from "zustand";
-import { devtools, subscribeWithSelector } from "zustand/middleware";
+import { devtools } from "zustand/middleware";
 
 import store from "./store";
 
 let aggregationStore = store;
 aggregationStore = devtools(aggregationStore, { name: "MiniDB Aggregation Store" });
-aggregationStore = subscribeWithSelector(aggregationStore);
 
 export const useAggregationStore = create(aggregationStore);
 
 // Export hooks
-
-// TODO: Triggers
-/*
-In useCollectionStore
-
-If entirecollection changes
-  - update whole collection tags
-  - update zipped tags
-If filtered collection changes
-  - update filtered collection tags
-  - update zipped collection tags
-*/
+export const useFilteredCollectionTags = () =>
+  useAggregationStore((state) => state.filteredCollectionTags);
+export const useEntireCollectionTags = () =>
+  useAggregationStore((state) => state.entireCollectionTags);
+export const useZippedCollectionTags = () =>
+  useAggregationStore((state) => state.zippedCollectionTags);
