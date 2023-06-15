2f7880ddd71c0bc94fe31a884ea9ad143e977b48
Dave Dankel <ddankel@gmail.com>
2023-06-15 15:39:19 -0500

    Insert placeholder hooks

--- src/modules/gallery/hooks/useAggregatedTags/index.js
+++ src/modules/gallery/hooks/useAggregatedTags/index.js
@@ -2,18 +2,32 @@ import { useEntireCollection, useFilteredCollection } from "@/common/hooks/useCo
 
 import zipTags from "../../utils/zipTags";
 import aggregateCollectionTags from "./utils/aggregateCollectionTags";
+import {
+  useEntireCollectionTags,
+  useZippedCollectionTags,
+  useFilteredCollectionTags as uFCT,
+} from "@/common/hooks/useAggregationStore";
 
 export const useFilteredCollectionTags = () => {
+  return uFCT();
+
+  console.log("useFilteredCollectionTags");
   const collection = useFilteredCollection();
   return aggregateCollectionTags(collection);
 };
 
 export const useWholeCollectionTags = () => {
+  return useEntireCollectionTags();
+
+  console.log("useWholeCollectionTags");
   const collection = useEntireCollection();
   return aggregateCollectionTags(collection);
 };
 
 export const useFilteredCollectionZippedTags = () => {
+  return useZippedCollectionTags();
+
+  console.log("useFilteredCollectionZippedTags");
   const filteredCollection = useFilteredCollection();
   const entireCollection = useEntireCollection();
 
